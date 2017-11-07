f851419f877884e3663d21b37c7b32f971d81753
Rahul Kumar <rahulcomp24@gmail.com>
2017-11-08 00:53:33 +0530

    Add getEventList API

---
+++ api/getEvents.php
@@ -0,0 +1,36 @@
+<?php
+    require_once __DIR__ . '/db_config.php';
+
+    function getEventsList() {
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE) or die(mysql_error());
+
+        $myquery = "SELECT `Title`, `Venue`, `FromDate`, `ToDate`, `ClubName` FROM events e, clubs c WHERE C.ClubID = e.ReportedBy";
+        $result = mysqli_query($con, $myquery);
+        $response['result'] = ['status' => NULL, 'message' => NULL, 'events' => array()];
+
+        if($result) {
+            if(mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $response['result']['status'] = 'success';
+                    array_push($response['result']['events'], $row);
+                }
+                mysqli_close($con);
+                return $response;
+            }
+        }
+        mysqli_close($con);
+        return NULL;
+    }
+
+    function getEventDetail($eventID) {
+        
+    }
+
+    if ($_GET['eventID']) {
+        echo json_encode(getEventDetail($_GET['eventID']));
+    }
+    else {
+        echo json_encode(getEventsList());
+    }
+
+?>
