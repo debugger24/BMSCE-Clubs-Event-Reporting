8e5fc2efe29a643a04db59cce81f89d3953aaec5
Rahul Kumar <rahulcomp24@gmail.com>
2017-11-08 00:53:56 +0530

    Add eventDetail API

--- api/getEvents.php
+++ api/getEvents.php
@@ -23,7 +23,25 @@
     }
 
     function getEventDetail($eventID) {
-        
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE) or die(mysql_error());
+
+        $myquery = "SELECT `Title`, `Venue`, `FromDate`, `ToDate`, `ClubName`, `Report` FROM events e, clubs c WHERE C.ClubID = e.ReportedBy AND e.eventID = $eventID";
+
+        $result = mysqli_query($con, $myquery);
+        $response['result'] = ['status' => NULL, 'message' => NULL, 'events' => array()];
+
+        if($result) {
+            if(mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $response['result']['status'] = 'success';
+                    array_push($response['result']['events'], $row);
+                }
+                mysqli_close($con);
+                return $response;
+            }
+        }
+        mysqli_close($con);
+        return NULL;
     }
 
     if ($_GET['eventID']) {
