18c1a886b6b99ac0886cb795d8a9fde4aa3bbdaf
Rahul Kumar <rahulcomp24@gmail.com>
2017-11-23 19:08:30 +0530

    Add logout function

--- js/userManage.js
+++ js/userManage.js
@@ -23,3 +23,27 @@ var login = function login() {
         }
     });
 }
+
+var logout = function logout() {
+    var navBtnLogin = $('#navBtnLogin');
+    var navUser = $('#navUser');
+    var navUserName = $('#navUserName');
+    var loginModal = $('#loginModal');
+
+    $.ajax({
+        url: "api/logout.php",
+        type: "post",
+        data: $('#loginForm').serialize(),
+        success: function(result){
+            result = JSON.parse(result);
+            if (result.status == "true") {
+                navBtnLogin.css("display", "block");
+                navUser.css("display", "none");
+                navUserName.html("");
+            }
+            else {
+                alert("Failed to logout");
+            }
+        }
+    });
+}
